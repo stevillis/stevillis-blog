2d05fd9a5bec51a2fa9f65aaebe677d6553f5b22
Stévillis Sousa <stevillis@hotmail.com>
2025-04-30 03:06:53 -0400

    refactor: rename django project

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for portfolio project.
+URL configuration for app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("__reload__/", include("django_browser_reload.urls")),
+    path("", include("home.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
