bea424f63e0a360dbf0c7ec8ea843648b972ad4e
Stévillis Sousa <stevillis@hotmail.com>
2025-05-01 00:31:27 -0400

    fix: add missing static root

--- app/settings.py
+++ app/settings.py
@@ -146,8 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-MEDIA_URL = "media/"
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, "staticfiles")
+
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if DEBUG:
