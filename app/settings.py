732b47c67f8c664bc42ffab148deed9c03c28041
Stévillis Sousa <stevillis@hotmail.com>
2025-05-07 02:19:23 -0400

    fix: update ALLOWED_HOSTS to use domain name

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "18.231.151.16"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "stevillis.com.br"]
 
 
 # Application definition
